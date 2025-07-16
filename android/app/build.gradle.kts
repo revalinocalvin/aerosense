dd1e3eaf4ba25fa4ccdcdb4f20869c61db336dde
revalinocalvin <revalinocalvin@gmail.com>
2025-07-17 02:15:35 +0700

    Added icons to app, and splash screen

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 android {
     namespace = "com.revalinocalvin.aerosense"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
